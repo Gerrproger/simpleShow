1626ed123b87665301563a6310da5db7cd6b347a
Evgenii <gerrproger@gmail.com>
2014-01-14 11:47:15 +0400

    Added directions & sliding effect

--- simpleShow.js
+++ simpleShow.js
@@ -14,16 +14,22 @@
       radios: true,
 	  arrows: true,
       effect: 'crossfade',
-	  cols: 10
+	  blocks: 10,
+      direction: 'bottom',
+      normIndex: 10,
+      maxIndex: 100
     }, options);
     return this.each(function(){
         var self = $(this),	
-        pref = '.simpleShow-',	
+        pref = '.simpleShow-',
+        act = 'simpleShow-active',	
         now = 1,
         t = null,
 		radios = null,
 		slides = self.find(pref+'slide'),
-		ln = slides.length;
+		ln = slides.length,
+        dr = set.direction,
+        n = set.blocks;
         if(ln < 2) return;
 		if(set.arrows) 
 		  self.find(pref+'controls').append('<a class="simpleShow-toLeft" href="#"></a>').append('<a class="simpleShow-toRight" href="#"></a>');
@@ -31,14 +37,17 @@
 		  self.find(pref+'controls').append('<div class="simpleShow-radios"></div>');
           for(var i=0; i<ln; i++) 
 		     self.find(pref+'radios').append('<a href="#" class="simpleShow-radio"></a>');
-          self.find(pref+'radio:first').addClass('simpleShow-active'); 
+          self.find(pref+'radio:first').addClass(act); 
 		  radios = self.find(pref+'radio');
 		}
+        slides.css({'overflow':'hidden', 'z-index':set.normIndex});
         function setInterv(){
 		   t = setInterval(function(){interv();}, set.interval+set.speed);
 		}		
         function interv(reverse, prev){
-            var nowN, nowP; 
+            var nowN, nowP,
+            w = self.width(),
+            h = self.height(); 
             if(now == ln){
                 now = nowN = 0;
                 nowP = reverse ? 1 : ln-1;
@@ -48,47 +57,69 @@
                 nowP = reverse ? (now+1==ln ? 0 : now+1) : now-1;
             } 
             if(prev != undefined) nowP = prev;
+            var elNnew = slides.eq(nowN),
+            elPrev = slides.eq(nowP);
             switch(set.effect){
                 case 'crossfade': {
-                    var s = Math.floor(set.speed / 2);
-                    slides.eq(nowN).fadeIn(s);
-                    slides.eq(nowP).fadeOut(s);
+                    var s = set.speed*0.5;
+                    elNnew.fadeIn(s);
+                    elPrev.fadeOut(s);
                 } break;
                 case 'fading': {
-                    var s = Math.floor(set.speed / 2);
-                    slides.eq(nowP).fadeOut(s, function(){
-                        slides.eq(nowN).fadeIn(s);
+                    var s = set.speed*0.5;
+                    elPrev.fadeOut(s, function(){
+                        elNnew.fadeIn(s);
                     }); 
                 } break;
+                case 'sliding': {
+                    var s = set.speed,
+                    b = (dr=='left' || dr=='right') ? w : h,
+                    rb = b*0.1;
+                    lt = (dr=='left' || dr=='right') ? (dr=='left' ? ('+='+rb) : ('-='+rb)) : 'auto',
+                    ltt = (dr=='left' || dr=='right') ? (dr=='left' ? ('-='+(b+rb)) : ('+='+(b+rb))) : 'auto',
+                    tt = (dr=='top' || dr=='bottom') ? (dr=='top' ? ('+='+rb) : ('-='+rb)) : 'auto',
+                    ttt = (dr=='top' || dr=='bottom') ? (dr=='top' ? ('-='+(b+rb)) : ('+='+(b+rb))) : 'auto';
+                    elPrev.css({'z-index':set.maxIndex, 'height':h, 'width':w}).
+                       animate({'left':lt, 'top':tt}, s*0.3).delay(s*0.1).
+                       animate({'left':ltt, 'top':ttt}, s*0.6, function(){
+                        $(this).css({'display':'none', 'z-index':set.normIndex, 'left':0, 'top':0, 'height':'auto', 'width':'auto'});
+                       });
+                    elNnew.css('display','block');
+                } break;
                 case 'blocks': {
-                    var w = Math.floor(self.width() / set.cols)+1,
-                    h = self.height(),
-                    ow = self.width(),
-                    el = slides.eq(nowP),
-                    s = Math.floor(set.speed / set.cols);
-                    for(var i=0; i<set.cols; i++){
+                    var ow = Math.floor(w / n)+1,
+                    w = w,
+                    h = h,
+                    oh = Math.floor(h / n)+1,
+                    el = elPrev,
+                    s = Math.floor(set.speed / n),
+                    fl = (dr=='bottom' || dr=='top'),
+                    drTop = fl ? ((dr=='top' ? -1 : 1) * h) : 'auto',
+                    drLeft = (dr=='left'|| dr=='right') ? ((dr=='left' ? -1 : 1) * w) : 'auto';
+                    for(var i=0; i<n; i++){
                         var block = $('<div>');
-                        block.width(w).height(h).
-                        css({'position':'absolute',
-                             'z-index': 100, 
-                             'top': 0, 
-                             'overflow': 'hidden', 
-                             'left': i*w+'px' }).
-                        html($(el).clone().css({'top': 0,
-                            'width': ow,
+                        block.css({'position':'absolute',
+                            'width': fl ? ow : w,
+                            'height': fl ? h : oh,
+                            'z-index': set.maxIndex, 
+                            'top': fl ? 0 : (i*oh+'px'), 
+                            'overflow': 'hidden', 
+                            'left': fl ? (i*ow+'px') : 0 }).
+                        html($(el).clone().css({'top': fl ? 0 : (-1*i*oh+'px'),
+                            'width': w,
                             'height': h, 
-                            'left': -1*i*w+'px' })
-                        ).appendTo(self); 
-                        block.delay(s*i).animate({top:h, opacity:0}, s*2, function(){
+                            'left': fl ? (-1*i*ow+'px') : 0 })
+                        ).appendTo(self);
+                        block.delay(s*i).animate({top:drTop, left:drLeft, opacity:0.2}, s*2, function(){
                                 $(this).remove();
                         });
                     }
-                    slides.eq(nowN).css('display','block');
-                    slides.eq(nowP).css('display','none');   
-                } break;
+                    elNnew.css('display','block');
+                    elPrev.css('display','none');   
+                }
             }
-            radios.eq(nowN).addClass('simpleShow-active');
-            radios.eq(nowP).removeClass('simpleShow-active');
+            radios.eq(nowN).addClass(act);
+            radios.eq(nowP).removeClass(act);
             now++;
         }
         setInterv();
@@ -107,10 +138,10 @@
         });
         self.find(pref+'radio').click(function(e){
             e.preventDefault();
-            if($(this).hasClass('simpleShow-active')) return;
+            if($(this).hasClass(act)) return;
             clearInterval(t);
             var ind = self.find(pref+'radio').index($(this)),
-                prev = now==1 ? 0 : now-1;
+            prev = now==1 ? 0 : now-1,
             now = ind;
             interv(false, prev);
             setInterv();
